ad2f6980ccf18c8fcbb22e2f7c7ef2fadf1cd0ad
pb-bj <pranish2017bajracharya@gmail.com>
2024-10-09 17:37:53 +0545

    update on app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,50 @@
-import { Outlet } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, Outlet } from 'react-router-dom';
 import { Navbar } from './components';
+import { menus } from './constants/menu';
 
 export default function App() {
+	const [activeLink, setActiveLink] = useState('');
 	return (
 		<>
 			<Navbar />
-			<main className="flex w-full pt-[64px]  bg-gray-100 text-black min-h-screen">
-				<div className="hidden bg-gray-400 md:flex">
-					<ul className="flex-col">
-						<li>Board</li>
-						<li>Discover</li>
-						<li>Library</li>
+			<main className="flex w-full pt-[40px] bg-bg1 text-p3 min-h-screen">
+				{/* larger screen */}
+				<div className="hidden sm:mt-5 sm:h-full sm:flex sm:flex-col sm:fixed sm:items-center sm:top-10 sm:bottom-0 bg-bg1 z-10">
+					<ul className="sm:pt-5 sm:pl-2">
+						{menus.map((menu) => (
+							<li key={menu.id} className="sm:px-1.5 sm:mt-5 sm:py-2 " onClick={() => setActiveLink(menu.id)}>
+								<Link
+									to={menu.route}
+									className={`sm:flex sm:flex-col sm:items-center sm:space-y-1 sm:w-full sm:h-full sm:text-center text-p7 cursor-pointer ${
+										activeLink === menu.id ? 'text-white' : 'text-p7'
+									}`}
+								>
+									<menu.icon />
+									<p className="text-[11px] font-medium">{menu.title}</p>
+								</Link>
+							</li>
+						))}
 					</ul>
 				</div>
-				<div className="flex-1 overflow-auto">
+				<div className="overflow-auto px-3 mt-9 sm:px-11 sm:ml-10 sm:mt-11">
 					<Outlet />
 				</div>
 			</main>
 			{/* bottom navbar */}
-			<nav className="fixed bottom-0 left-0 z-50  md:hidden bg-green-600 text-white w-full">
-				<ul className="flex justify-around p-2">
-					<li>Board</li>
-					<li>Discover</li>
-					<li>Library</li>
+			<nav className="fixed bottom-0 left-0 z-50 w-full bg-bg1 px-6 py-3 sm:hidden">
+				<ul className="flex items-baseline space-y-2 justify-around">
+					{menus.map((menu) => (
+						<li key={menu.id} className="" onClick={() => setActiveLink(menu.id)}>
+							<Link
+								to={menu.route}
+								className={`flex flex-col items-center text-center px-3.5 py-2 cursor-pointer ${activeLink === menu.id ? 'text-white' : 'text-p7'}`}
+							>
+								<menu.icon />
+								<span className="text-[11px] ">{menu.title}</span>
+							</Link>
+						</li>
+					))}
 				</ul>
 			</nav>
 		</>
--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -2,14 +2,14 @@ import { Search } from 'lucide-react';
 
 export default function SearchBox() {
 	return (
-		<form className="flex justify-center items-center">
+		<form className="flex justify-center items-center   ">
 			<input
 				type="text"
 				placeholder="search movies, tv shows..."
-				className="w-[220px] border border-r-0  border-gray-300 tracking-tight px-3 py-2 rounded-tl-lg rounded-bl-lg focus:outline-none text-black text-sm"
+				className="w-[200px] sm:w-[300px] h-[41px] border border-r-0  border-none tracking-normal px-6 py-3 bg-bg2 text-p2 rounded-tl-2xl rounded-bl-2xl focus:outline-none text-[12.5px]"
 			/>
-			<div className="flex border border-l-0 py-1.5 px-3 rounded-tr-lg rounded-br-lg bg-white text-black ">
-				<Search className="w-[20px] cursor-pointer  " />
+			<div className="h-[41px] flex border border-l-0 py-1.5 px-3 rounded-tr-2xl border-none rounded-br-2xl bg-bg2 text-p2">
+				<Search className="w-[20px]  cursor-pointer  " />
 			</div>
 		</form>
 	);
