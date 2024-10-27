a656d682b7180a4d12fdfe322facf51f0d1b1144
pb-bj <pranish2017bajracharya@gmail.com>
2024-10-27 14:03:58 +0545

    sudden changes to genre component

--- src/components/GenreComponent.tsx
+++ src/components/GenreComponent.tsx
@@ -3,7 +3,7 @@ type GenreProps = {
 };
 
 const GenreComponent = ({ name }: GenreProps) => {
-	return <li className="bg-p6 px-4 py-1 rounded-xl text-center cursor-pointer hover:bg-p4">{name}</li>;
+	return <li className="backdrop-blur-xl bg-white/30 px-4 py-1 rounded-xl text-center cursor-pointer hover:bg-p4">{name}</li>;
 };
 
 export default GenreComponent;
