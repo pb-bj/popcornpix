6dc6bd4b9f4528c977d6991f41db5910869e3912
pb-bj <pranish2017bajracharya@gmail.com>
2024-12-17 21:07:18 +0545

    minor change

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -5,7 +5,7 @@ import { Movie } from '../types/Movie';
 import { Series } from '../types/Series';
 
 type ContentProps<T extends Movie | Series> = {
-	label: string;
+	label?: string;
 	items: T[];
 	render: (items: T) => ReactNode;
 	maxItems?: number; // for maximun items to be display
@@ -18,12 +18,16 @@ const Content = <T extends Movie | Series>({ label, items, render, maxItems }: C
 		<div className="mb-8">
 			<div className="flex justify-between items-baseline pb-4">
 				<div className="text-primary ">{label}</div>
-				<div className="flex items-center justify-between w-[80px] text-sm text-p2 cursor-pointer px-1.5 sm:text-secondary sm:hover:text-white sm:hover:bg-p4 sm:hover:rounded-xl sm:hover:shadow-lg">
-					<div>See All</div>
-					<div>
-						<ChevronRight width={20} />
-					</div>
-				</div>
+				{maxItems && (
+					<>
+						<div className="flex items-center justify-between w-[80px] text-sm text-p2 cursor-pointer px-1.5 sm:text-secondary sm:hover:text-white sm:hover:bg-p4 sm:hover:rounded-xl sm:hover:shadow-lg">
+							<div>See All</div>
+							<div>
+								<ChevronRight width={20} />
+							</div>
+						</div>
+					</>
+				)}
 			</div>
 			<ul className="grid gap-4 grid-cols-3 sm:grid-cols-3 md:grid-cols-6 md:gap-4">
 				{displayItems && displayItems?.length > 0 ? (
