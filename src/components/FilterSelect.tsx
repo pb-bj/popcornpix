5d8266e437def2f8fa98a7fc6d47147c981138c8
pb-bj <pranish2017bajracharya@gmail.com>
2025-01-21 18:37:50 +0545

    filter select component created

---
+++ src/components/FilterSelect.tsx
@@ -0,0 +1,29 @@
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+
+type FilterSelectProps = {
+	value: string;
+	onHandleMedia: (value: string) => void;
+	placeholder: string;
+	items: { id: number; title: string }[];
+};
+
+const FilterSelect = ({ value, onHandleMedia, placeholder, items }: FilterSelectProps) => {
+	return (
+		<>
+			<Select value={value} onValueChange={onHandleMedia}>
+				<SelectTrigger className="w-[180px] border-none rounded-full bg-bg4 text-xs">
+					<SelectValue placeholder={placeholder} />
+				</SelectTrigger>
+				<SelectContent className="shadow-xl cursor-pointer border-none">
+					{items.map((item) => (
+						<SelectItem key={item.id} value={item.title} className="cursor-pointer text-xs">
+							{item.title}
+						</SelectItem>
+					))}
+				</SelectContent>
+			</Select>
+		</>
+	);
+};
+
+export default FilterSelect;
